1689b4e18edd2f9dd4e2db4d22cc298f7ff9cde4
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-25 11:44:04 -0300

    Add Logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
   return(
     <div className='Header'>
         <div className='Search'>
+            <h1>Logo</h1>
             <input type={'text'} placeholder={'Search'}/>
         </div>
     </div>
